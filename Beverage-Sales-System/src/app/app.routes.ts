dff68123f65d574d1ae9ad5d971e920b1a8868f0
Kirils Lusnickis <kirils.lusnickis@discovercarhire.com>
2024-11-16 10:50:40 +0200

    Refactor app routing module to use separate file for routes

--- Beverage-Sales-System/src/app/app.routes.ts
+++ Beverage-Sales-System/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DrinkListComponent } from './drink-list/drink-list.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ReturnComponent } from './return/return.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DrinkListComponent },
+    { path: 'payment', component: PaymentComponent },
+    { path: 'return', component: ReturnComponent }
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
